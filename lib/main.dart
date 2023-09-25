f46ca114752074b798c0cfb07e8d65906dacf3db
peacebefore <peacebefore@gmail.com>
2023-09-25 14:46:16 -0700

    complete example canvas drawing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +31,35 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text("Art Class"),
       ),
-      body: const Center(child: Text("hello")),
+      body: CustomPaint(
+        painter: MyPainter(),
+        size: MediaQuery.of(context).size,
+      ),
     );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint linePaint = Paint()..strokeWidth = 10;
+    canvas.drawLine(const Offset(50, 150), const Offset(150, 220), linePaint);
+    canvas.drawLine(
+        Offset(size.width - 50, 150), Offset(size.width - 150, 220), linePaint);
+
+    Paint circlePaint = Paint();
+    canvas.drawCircle(Offset(100, 250), 20, circlePaint);
+    canvas.drawCircle(Offset(size.width - 100, 250), 20, circlePaint);
+
+    Paint arcPaint = Paint()
+      ..strokeWidth = 10
+      ..style = PaintingStyle.stroke;
+    Rect rect = const Rect.fromLTWH(105, 350, 220, 300);
+    canvas.drawArc(rect, math.pi, math.pi, true, arcPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
